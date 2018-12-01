b69a1920c04976856a7019d072453bb018a21765
Kirill Korepanov <kirill.korepanov@ika.de>
2018-12-02 00:02:09 +0100

    Fail scene

--- js/scenes/failScene.js
+++ js/scenes/failScene.js
@@ -6,10 +6,17 @@ class FailScene extends SceneBase {
         restartButton.position.set(Params.application.width - 120, Params.application.height - 50);
         restartButton.onClick(() => this.switchCallback(Params.sceneType.START, {}));
         this.addChild(restartButton);
+
+        this.reasonText = new PIXI.Text("???", Params.textStyle.loseDescription);
+        this.reasonText.anchor.set(0.5);
+        this.reasonText.position.set(Params.application.width / 2, Params.application.height /  2);
+        this.addChild(this.reasonText);
     }
 
     init(data) {
         super.init(data);
+
+        this.reasonText.text = data.reason;
     }
 
     update(deltaTime) {
--- js/scenes/mainScene.js
+++ js/scenes/mainScene.js
@@ -1,15 +1,15 @@
 class MainScene extends SceneBase {
     constructor(switchCallback) {
         super(switchCallback, Params.textures.background.start, "Thats a game!");
-        
+
         let finishButton = new TextButton("Finish game");
         finishButton.position.set(Params.application.width - 120, Params.application.height - 50);
         finishButton.onClick(() => this.switchCallback(Params.sceneType.FINISH, {}));
         this.addChild(finishButton);
-        
+
         let failButton = new TextButton("Fail game");
         failButton.position.set(Params.application.width - 120, Params.application.height - 100);
-        failButton.onClick(() => this.switchCallback(Params.sceneType.FAIL, {}));
+        failButton.onClick(() => this.switchCallback(Params.sceneType.FAIL, { reason: "You just suck..." }));
         this.addChild(failButton);
     }
 
